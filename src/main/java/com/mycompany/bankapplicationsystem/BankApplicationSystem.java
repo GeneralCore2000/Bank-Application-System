eca2bd789b2824416777785923de7249038e471d
John Lloyd <92360974+GeneralCore2000@users.noreply.github.com>
2024-10-31 19:13:27 +0800

    Removed comments

--- src/main/java/com/mycompany/bankapplicationsystem/BankApplicationSystem.java
+++ src/main/java/com/mycompany/bankapplicationsystem/BankApplicationSystem.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.mycompany.bankapplicationsystem;
 
-/**
- *
- * @author School
- */
 public class BankApplicationSystem {
 
     public static void main(String[] args) {
